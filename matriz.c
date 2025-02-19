1203959f1dde42088ca4010284327b62c87aed2c
geizacs <51997261+geizacs@users.noreply.github.com>
2025-02-18 21:57:29 -0300

    Add files via upload

---
+++ matriz.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "matriz.h"
+
+struct matriz {
+    int lin;
+    int col;
+    int** v;
+};
+
+
+float acessa_matriz (Matriz *mat, int i, int j) {
+
+  return mat->v[i][j];
+
+}
+
+// Atribui v a um elemento da matriz dado por i e j
+void atribui_matriz (Matriz *mat, int i, int j, int v) {
+
+  mat->v[i][j] = v;
+
+}
+
+int** aloca_matriz (int n_lin, int n_col) {
+  int i;
+
+  int** mat = (int**) malloc (n_lin * sizeof(int*));
+  for (i=0;i < n_lin;i++)
+      mat[i] = (int*) malloc (n_col * sizeof(int));
+
+  return mat;
+}
+
+Matriz* mat_cria (int m, int n) {
+    Matriz* mat =  (Matriz*) malloc(sizeof(Matriz));
+
+    mat->v = aloca_matriz(m, n);
+    mat->lin = m;
+    mat->col = n;
+
+    return mat;
+}
+
+/*void imprime_matriz (int** mat, int n_lin, int n_col) {
+
+  int i,j;
+  for (i=0;i < n_lin;i++) {
+    for (j=0;j < n_col;j++)
+      printf ("%d ", mat[i][j]);
+    printf ("\n");
+  }
+}
+
+void imprime_matriz_arquivo (int** mat, int n_lin, int n_col) {
+  FILE *sai;
+  sai = fopen ("arqsaida.txt", "w");
+  //Verifica se arquivo de saida foi aberto corretamente
+  if (!sai) {
+    printf ("Erro na abertura de saida.txt");
+    exit (1);
+  }
+  int i,j;
+  for (i=0;i < n_lin;i++) {
+    for (j=0;j < n_col;j++)
+      fprintf (sai,"%d ", mat[i][j]);
+    fprintf (sai,"\n");
+  }
+}
+*/
+
+void libera_matriz (Matriz* mat, int n_lin) {
+
+  int i;
+
+  for (i=0;i < n_lin;i++)
+      free (mat->v[i]);
+  free (mat);
+
+}
---
+++ matriz.h
@@ -0,0 +1,15 @@
+typedef struct matriz Matriz;
+
+int** aloca_matriz (int n_lin, int n_col);
+
+Matriz* mat_cria (int m, int n) ;
+
+void atribui_matriz (Matriz* mat, int i, int j, int v);
+
+void imprime_matriz (Matriz* mat, int n_lin, int n_col);
+
+//void imprime_matriz_arquivo (int** mat, int n_lin, int n_col);
+
+void libera_matriz (Matriz *mat, int n_lin);
+
+//void set_matriz (int** mat, int n_lin, int n_col, int valor);
